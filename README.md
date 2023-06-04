b486f0016b9c275df5a65d43d0a265676247b7e9
Dean Thompson <deanthompson.music@gmail.com>
2023-06-04 17:54:38 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# CenterOriginSlider 
+
+CenterOriginSlider is an open-source SwiftUI package that provides a customizable center origin slider for your iOS projects. This slider allows users to select values in a range, either negative or positive, from a center origin point.
+
+This package provides a variety of customization options such as thumb size, color, guide bar style, tracking bar color, and more, making it a flexible choice for your user interface needs.
+
+## Features
+
+- Set the minimum and maximum values for your slider.
+- Opt to increment values discretely or continuously.
+- Customize the slider's thumb size, color, and shadow.
+- Style the guide bar with your choice of corner radius, color, and height.
+- Define the appearance of the tracking bar, including its color and height.
+
+## Requirements
+
+- iOS 13.0+
+- mac OS 11.0+
+
+## Installation
+
+CenterOriginSlider is available through the Swift Package Manager. 
+
+To add CenterOriginSlider to your Xcode project:
+1. Select File > New > Package...
+2. Enter `https://github.com/thompson-dean/CenterOriginSlider.git` into the package repository URL text box.
+3. Follow the prompts to add the package to your project.
+
+## Usage
+
+First, import the `CenterOriginSlider` package in the file where you want to use it:
+
+```swift
+import SwiftUI
+import CenterOriginSlider
+```
+
+```
+struct ContentView: View {
+    @State private var sliderValue: Float = 0.0
+
+    var body: some View {
+        CenterOriginSlider(
+            minValue: -100,
+            maxValue: 100,
+            sliderValue: $sliderValue
+        )
+        .frame(width: 300, height: 60)
+        .padding()
+    }
+}
+```
+
+In this example, the slider's value can vary from -100 to 100, starting from 0.
+
+To further customize the slider, you can specify other properties as per your needs. For example:
+
+```
+CenterOriginSlider(
+    minValue: -50,
+    maxValue: 50,
+    increment: 10,
+    sliderValue: $sliderValue,
+    thumbSize: 24,
+    thumbColor: .red,
+    guideBarCornerRadius: 4,
+    guideBarColor: .blue.opacity(0.2),
+    guideBarHeight: 6,
+    trackingBarColor: .blue,
+    trackingBarHeight: 6,
+    shadow: 2
+)
+```
+## Contribution
+Contributions to the CenterOriginSlider project are welcome! Feel free to open a new issue or send a pull request, if you happen to find a bug, or would liek to add any new features.
+
+## License
+CenterOriginSlider is available under the MIT license.
---
+++ Sources/CenterOriginSlider/CenterOriginSlider.swift
@@ -0,0 +1,161 @@
+// MIT License
+//
+// Copyright (c) 2023 Dean Thompson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+/// `CenterOriginSlider` is a custom SwiftUI view that represents a slider. The slider's thumb can be moved horizontally
+/// to select a value within a range. The slider's appearance and behavior can be customized with several options.
+///
+import SwiftUI
+
+public struct CenterOriginSlider: View {
+
+    /// The minimum value the slider can take.
+    public let minValue: Float
+
+    /// The maximum value the slider can take.
+    public let maxValue: Float
+
+    /// The increment by which the value should change. If this is nil, the value changes continuously.
+    public let increment: Float?
+
+    /// A binding to a variable that holds the current slider value.
+    @Binding public var sliderValue: Float
+
+    /// The size of the slider's thumb.
+    public let thumbSize: CGFloat
+
+    /// The color of the slider's thumb.
+    public let thumbColor: Color
+
+    /// The corner radius of the slider's guide bar.
+    public let guideBarCornerRadius: CGFloat
+
+    /// The color of the slider's guide bar.
+    public let guideBarColor: Color
+
+    /// The height of the slider's guide bar.
+    public let guideBarHeight: CGFloat
+
+    /// The color of the slider's tracking bar.
+    public let trackingBarColor: Color
+
+    /// The height of the slider's tracking bar.
+    public let trackingBarHeight: CGFloat
+
+    /// The shadow radius of the slider's thumb.
+    public let shadow: Int
+    
+    @State public var accumulatedWidth: CGFloat = 0
+    @State public var offset: CGSize = .zero
+    
+    public init(
+        minValue: Float,
+        maxValue: Float,
+        increment: Float? = nil,
+        sliderValue: Binding<Float>,
+        thumbSize: CGFloat = 16,
+        thumbColor: Color = .white,
+        guideBarCornerRadius: CGFloat = 2,
+        guideBarColor: Color = .white.opacity(0.15),
+        guideBarHeight: CGFloat = 4,
+        trackingBarColor: Color = .white,
+        trackingBarHeight: CGFloat = 4,
+        shadow: Int = 0
+    ) {
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.increment = increment
+        self._sliderValue = sliderValue
+        self.thumbSize = thumbSize
+        self.thumbColor = thumbColor
+        self.guideBarCornerRadius = guideBarCornerRadius
+        self.guideBarColor = guideBarColor
+        self.guideBarHeight = guideBarHeight
+        self.trackingBarColor = trackingBarColor
+        self.trackingBarHeight = trackingBarHeight
+        self.shadow = shadow
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            let sliderWidth = geometry.size.width - 32
+            let valueRange = maxValue - minValue
+            let dragGesture = DragGesture()
+                .onChanged({ value in
+                    let width = value.translation.width + accumulatedWidth
+                    let limitedWidth = max(min(width, sliderWidth / 2 - thumbSize / 2), -sliderWidth / 2 + thumbSize / 2)
+                    offset.width = limitedWidth
+
+                    let sliderProgress = (limitedWidth + sliderWidth / 2 - thumbSize / 2) / (sliderWidth - thumbSize)
+                    print("DEBUG: SLIDER RPOGRESS \(sliderProgress)")
+                    let rawValue = minValue + Float(sliderProgress) * valueRange
+                    print("DEBUG: RawValue \(rawValue)")
+                    
+                    if let unwrappedIncrement = increment {
+                        sliderValue = round(rawValue / unwrappedIncrement) * unwrappedIncrement
+                    } else {
+                        sliderValue = round(rawValue)
+                    }
+                })
+                .onEnded({ value in
+                    accumulatedWidth = offset.width
+                })
+            VStack(alignment: .center) {
+                ZStack(alignment: .center) {
+                    RoundedRectangle(cornerRadius: guideBarCornerRadius)
+                        .frame(width: sliderWidth, height: guideBarHeight)
+                        .foregroundColor(guideBarColor)
+                    HStack(spacing: 0) {
+                        HStack {
+                            Spacer()
+                            Rectangle()
+                                .frame(width: max(sliderValue < 0 ? CGFloat((-sliderValue / valueRange) * Float(sliderWidth - thumbSize)) : 0, 0), height: trackingBarHeight)
+                                .foregroundColor(trackingBarColor)
+                        }
+                        .frame(maxWidth: .infinity)
+                        HStack {
+                            Rectangle()
+                                .frame(width: max(sliderValue > 0 ? CGFloat((sliderValue / valueRange) * Float(sliderWidth - thumbSize)) : 0, 0), height: trackingBarHeight)
+                                .foregroundColor(trackingBarColor)
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: .infinity)
+                    Circle()
+                        .frame(width: thumbSize, height: thumbSize)
+                        .foregroundColor(thumbColor)
+                        .offset(offset)
+                        .gesture(dragGesture)
+                        .onChange(of: sliderValue) { value in
+                            let sliderProgress = (value - minValue) / valueRange
+                            let newWidth = CGFloat(sliderProgress) * (sliderWidth - thumbSize) - sliderWidth / 2 + thumbSize / 2
+                            offset.width = newWidth
+                        }
+                }
+                .padding(.horizontal, 16)
+            }
+            .frame(maxHeight: .infinity)
+        }
+    }
+}
+
+
